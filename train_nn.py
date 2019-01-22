cd401e5e2606d04a840beab13596afc2e1997a74
yy1252450987 <1252450987@qq.com>
2019-01-22 10:34:52 +0800

    Update train_nn.py
    code annotation

--- train_nn.py
+++ train_nn.py
@@ -13,20 +13,20 @@ from nn import create_net
 @click.option('--weights_from', default=None, show_default=True,
               help='Path to initial weights file.')
 def main(cnf, weights_from):
-
+    # 配置参数
     config = util.load_module(cnf).config
-
+    # 网络参数路径
     if weights_from is None:
         weights_from = config.weights_file
     else:
         weights_from = str(weights_from)
-
+    # 导入训练文件名,以及他们对应的LABEL 
     files = data.get_image_files(config.get('train_dir'))
     names = data.get_names(files)
     labels = data.get_labels(names).astype(np.float32)
-
+    # 搭建好网络
     net = create_net(config)
-
+    # 导入训练好的参数到搭建好的网络当中
     try:
         net.load_params_from(weights_from)
         print("loaded weights from {}".format(weights_from))
@@ -34,6 +34,7 @@ def main(cnf, weights_from):
         print("couldn't load weights starting from scratch")
 
     print("fitting ...")
+    # 训练网络
     net.fit(files, labels)
 
 if __name__ == '__main__':
